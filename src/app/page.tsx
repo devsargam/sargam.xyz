a9785e75852d2a9b0956058dd198a64b376c18ba
devsargam <sargampoudel100@gmail.com>
2024-02-28 21:07:58 +0530

    feat: setup bg and text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <main>Sargam Poudel</main>;
+  return (
+    <div className="min-h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.1] bg-grid-black/[0.1] relative flex items-center justify-center">
+      <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+        Sargam Poudel
+      </p>
+    </div>
+  );
 }
