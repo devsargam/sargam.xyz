4caf6d91f1067158f94216c6233da1f1e0a1db6c
Sargam <sargampoudel100@gmail.com>
2024-10-12 10:23:10 +0545

    fix: add introduction component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { AnimatedCursor } from '@/components';
-import { Nav } from '@/components';
-import { Toaster } from 'sonner';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className=" dark:bg-black bg-white dark:bg-grid-small-white/[0.3] sm:dark:bg-grid-white/[0.1] bg-grid-black/[0.1]">
+        <main className="">
           {children}
           <SpeedInsights />
           <Analytics />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import Introduction from '@/components/introduction';
+
 export default function Home() {
-  return <></>;
+  return (
+    <main className="flex flex-col bg-black justify-center items-start max-w-2xl mx-auto mb-16 border-white border-2">
+      {/* Introduction Section */}
+      <Introduction />
+      {/* Proof of work Section */}
+      {/* Experience Section */}
+      {/* Footer Section */}
+    </main>
+  );
 }
