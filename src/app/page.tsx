8476354a4761c9a0cc43d49bebcba9261fe901cb
Sargam <sargampoudel100@gmail.com>
2024-10-13 18:04:04 +0545

    feat: add work experience section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Introduction from '@/components/introduction';
 import ProofOfWork from '@/components/proof-of-work';
+import WorkExperience from '@/components/work-experience';
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 dark:border-white border-zinc-900 border-2 px-4 py-4">
+    <main className="flex flex-col gap-y-8 justify-center items-start max-w-2xl mx-auto mb-16 dark:border-white border-zinc-900 border-2 px-4 py-4">
       {/* Introduction Section */}
       <Introduction />
       {/* Proof of work Section */}
       <ProofOfWork />
       {/* Experience Section */}
+      <WorkExperience />
       {/* Footer Section */}
     </main>
   );
---
+++ src/components/work-experience.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function WorkExperience() {
+  return (
+    <div className="flex flex-col gap-y-4 text-black dark:text-white w-full">
+      <h2 className="font-bold text-2xl md:text-4xl tracking-tight">
+        Work Experience
+      </h2>
+      <p className="text-gray-600 dark:text-gray-400 mb-4">
+        All my experiences as a software engineer
+      </p>
+    </div>
+  );
+}
+
+export default WorkExperience;
