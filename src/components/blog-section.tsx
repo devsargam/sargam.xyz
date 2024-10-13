3cd3d108f8d0680223f8b851fc5c03f4bc8d0b6c
Sargam <sargampoudel100@gmail.com>
2024-10-13 20:56:42 +0545

    fix: spacing issues and small nits

--- src/components/blog-section.tsx
+++ src/components/blog-section.tsx
@@ -7,7 +7,7 @@ function BlogSection() {
     <div className="flex flex-col gap-y-4 text-black dark:text-white w-full">
       <h2 className="font-bold text-2xl md:text-4xl tracking-tight">Blogs</h2>
       {mockBlogs.length > 0 ? null : (
-        <p className="text-gray-600 dark:text-gray-400 mb-4">
+        <p className="text-gray-600 dark:text-gray-400">
           I haven&apos;t written any blogs yet but I do plan to write some
           soon...
         </p>
