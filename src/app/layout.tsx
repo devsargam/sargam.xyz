cbbf75754ce2f626eb3ef996a4d74000c55ebea1
Sargam <sargampoudel100@gmail.com>
2024-11-08 08:43:58 +0545

    feat: add tracking with microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,6 +25,15 @@ export default function RootLayout({
           <SpeedInsights />
           <Analytics />
         </main>
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "nczs4rjeot");
+          `}
+        </Script>
       </body>
     </html>
   );
